8165a070f3f9e6867b3de4d4f51e28000e320219
yassine achehboune <44009319+SakoDev@users.noreply.github.com>
2024-09-14 13:18:01 +0800

    modified:   app/Actions/Fortify/UpdateUserProfileInformation.php 	modified:   resources/views/layouts/app.blade.php 	modified:   resources/views/layouts/guest.blade.php 	modified:   resources/views/profile/update-profile-information-form.blade.php

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Laravel\Fortify\Contracts\UpdatesUserProfileInformation;
 
+
 class UpdateUserProfileInformation implements UpdatesUserProfileInformation
 {
     /**
@@ -17,23 +18,41 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
      */
     public function update(User $user, array $input): void
     {
+        // Validate the new fields along with existing ones
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
             'photo' => ['nullable', 'mimes:jpg,jpeg,png', 'max:1024'],
+            'ice' => ['required', 'string', 'max:255'],
+            'company_name' => ['required', 'string', 'max:255'],
+            'address' => ['required', 'string', 'max:255'],
+            'phone_number' => ['required', 'string', 'max:20'],
+            'patente' => ['required', 'string', 'max:255'],
+            'id_fiscale' => ['required', 'string', 'max:255'],
         ])->validateWithBag('updateProfileInformation');
 
+        // If a profile photo is uploaded, update it
         if (isset($input['photo'])) {
             $user->updateProfilePhoto($input['photo']);
         }
 
-        if ($input['email'] !== $user->email &&
-            $user instanceof MustVerifyEmail) {
+        // If the email has changed, handle email verification
+        if (
+            $input['email'] !== $user->email &&
+            $user instanceof MustVerifyEmail
+        ) {
             $this->updateVerifiedUser($user, $input);
         } else {
+            // Force fill and save the updated fields
             $user->forceFill([
                 'name' => $input['name'],
                 'email' => $input['email'],
+                'ice' => $input['ice'],
+                'company_name' => $input['company_name'],
+                'address' => $input['address'],
+                'phone_number' => $input['phone_number'],
+                'patente' => $input['patente'],
+                'id_fiscale' => $input['id_fiscale'],
             ])->save();
         }
     }
@@ -45,12 +64,21 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
      */
     protected function updateVerifiedUser(User $user, array $input): void
     {
+        // If the user needs to verify the email, clear the email verification timestamp
         $user->forceFill([
             'name' => $input['name'],
             'email' => $input['email'],
             'email_verified_at' => null,
+            'ice' => $input['ice'],
+            'company_name' => $input['company_name'],
+            'address' => $input['address'],
+            'phone_number' => $input['phone_number'],
+            'patente' => $input['patente'],
+            'id_fiscale' => $input['id_fiscale'],
         ])->save();
 
+        // Send email verification notification
         $user->sendEmailVerificationNotification();
     }
 }
+
